943e557f5d8fe12616f1de478323a32263cff391
Ron Nilekani <nilekani.raunaq@gmail.com>
2014-09-22 13:23:07 -0500

    Create email_helper.py

---
+++ email_helper.py
@@ -0,0 +1,42 @@
+'''
+This script shows how to send emails via Python.
+'''
+
+def send_mail(recipient, subject, message, sender):
+    '''
+    Simple function to help simplify sending SMTP email
+
+    Assumes a mailserver is available on localhost
+    '''
+
+    import smtplib
+    from email.mime.text import MIMEText
+
+    message = MIMEText(message)
+    message['Subject'] = subject
+    message['From'] = sender
+    message['To'] = recipient
+
+    # Create SMTP connection object to localhost
+    smtp_conn = smtplib.SMTP('localhost')
+
+    # Send the email
+    smtp_conn.sendmail(sender, recipient, message.as_string())
+
+    # Close SMTP connection
+
+    smtp_conn.quit()
+
+    return True
+
+
+if __name__ == '__main__':
+	
+	recipient = 'nilekani.raunaq@gmail.com'
+	subject = 'Test message'
+	message = '''
+  This is a fictional message
+
+Regards,
+Ron
+'''	
